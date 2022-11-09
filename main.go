8737ea284555416e38c4e928b3bd5e07fa7d6f94
Michael <saenz.michael@heb.com>
2022-11-09 00:01:28 -0600

    testing kubectl response data with fyne

--- main.go
+++ main.go
@@ -7,8 +7,7 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
-	v1 "k8s.io/api/apps/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
@@ -17,38 +16,47 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("KUI")
 
+	//ctx := context.Background()
+	config := ctrl.GetConfigOrDie()
+	clientset := kubernetes.NewForConfigOrDie(config)
+
+	//namespace := "kube-system"
+
+	// uses the current context in kubeconfig
+	// path-to-kubeconfig -- for example, /root/.kube/config
+	//config, _ := clientcmd.BuildConfigFromFlags("", "<path-to-kubeconfig>")
+	// creates the clientset
+	//clientset, _ := kubernetes.NewForConfig(config)
+	// access the API to list pods
+	pods, _ := clientset.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{})
+	fmt.Print(pods)
+	//pod_msg, _ := fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	pod_msg := fmt.Sprintf("There are %d pods in the cluster\n", len(pods.Items))
+	// items, err := GetDeployments(clientset, ctx, namespace)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	for _, item := range items {
+	// 		fmt.Printf("%+v\n", item)
+	// 	}
+	// }
 	hello := widget.NewLabel("KUI!")
 	w.SetContent(container.NewVBox(
 		hello,
 		widget.NewButton("run", func() {
-			hello.SetText("nothing to see here :)")
+			hello.SetText(pod_msg)
 		}),
 	))
-
-	ctx := context.Background()
-	config := ctrl.GetConfigOrDie()
-	clientset := kubernetes.NewForConfigOrDie(config)
-
-	namespace := "kube-system"
-	items, err := GetDeployments(clientset, ctx, namespace)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		for _, item := range items {
-			fmt.Printf("%+v\n", item)
-		}
-	}
-
 	w.ShowAndRun()
 }
 
-func GetDeployments(clientset *kubernetes.Clientset, ctx context.Context,
-	namespace string) ([]v1.Deployment, error) {
+// func GetDeployments(clientset *kubernetes.Clientset, ctx context.Context,
+// 	namespace string) ([]v1.Deployment, error) {
 
-	list, err := clientset.AppsV1().Deployments(namespace).
-		List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
-	return list.Items, nil
-}
+// 	list, err := clientset.AppsV1().Deployments(namespace).
+// 		List(ctx, metav1.ListOptions{})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return list.Items, nil
+// }
